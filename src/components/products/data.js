324c5fc9ab5b7d2ab87c14f149d876c7db0e0306
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-02-27 15:58:57 +0200

    looking better

--- src/components/products/data.js
+++ src/components/products/data.js
@@ -1,16 +1,19 @@
 import product1 from '../../images/burger1.jpg'
 import product2 from '../../images/burger3.jpg'
 import product3 from '../../images/burger5.jpg'
+import product4 from '../../images/ice-crream.jpg'
+import product5 from '../../images/cookies.jpg'
+import product6 from '../../images/dn.jpg'
 
 export const productData = [
     {img: product1, alt: 'burger', name:"Queen Burger", desc:'Bacon and cheese burger with lettuce, tomato, onions and pickles', price:'R89.99', button:'Add to Cart'},
     {img: product2, alt: 'burger', name:"Impossible Burger", desc:'World renowned plant based burger patty with lettuce, tomato, onions and pickles', price:'R119.99', button:'Add to Cart'} ,
-    {img: product3, alt: 'burger', name:"King Burger", desc:'Bacon and cheese burger, tripple patty with lettuce, tomato, pickles and signature sauce', price:'R89.99', button:'Add to Cart'}
+    {img: product3, alt: 'burger', name:"Early Riser", desc:' 200g patty topped with a fried egg and cheese with lettuce tomato', price:'R74.99', button:'Add to Cart'}
 ]
 
 
 export const productDataTwo = [
-    {img: product1, alt: 'burger', name:"Queen Burger", desc:'Bacon and cheese burger with lettuce, tomato, onions and pickles', price:'R89.99', button:'Add to Cart'},
-    {img: product2, alt: 'burger', name:"Impossible Burger", desc:'World renowned plant based burger patty with lettuce, tomato, onions and pickles', price:'R119.99', button:'Add to Cart'} ,
-    {img: product3, alt: 'burger', name:"King Burger", desc:'Bacon and cheese burger, tripple patty with lettuce, tomato, pickles and signature sauce', price:'R89.99', button:'Add to Cart'}
+    {img: product4, alt: 'burger', name:"Ice cream", desc:'Choose from a range of our homemade flavours.', price:'R89.99', button:'Add to Cart'},
+    {img: product5, alt: 'burger', name:"Choc Chip Cookies", desc:'Sold by the dozen, the whole family is in for a treat.', price:'R119.99', button:'Add to Cart'} ,
+    {img: product6, alt: 'burger', name:"Doughnuts", desc:'A twelve pack of doughnuts is easy to share and delicious.', price:'R89.99', button:'Add to Cart'}
 ]
